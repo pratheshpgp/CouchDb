f8691e2049eed00b00649ad687ae242b55323e36
pratheshpgp <45036226+pratheshpgp@users.noreply.github.com>
2018-11-14 17:49:33 +0530

    First_Inital Commit
    First_Inital Commit

---
+++ CouchDb/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace CouchDb
+{
+    internal class Person
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ CouchDb/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using MyCouch;
+
+namespace CouchDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var client = new MyCouchClient("http://localhost:5984/", "test"))
+            {
+
+                //POST with server generated id
+                client.Documents.PostAsync("{\"name\":\"Daniel\"}");
+
+                //PUT for client generated id
+                 client.Documents.PutAsync("1", "{\"name\":\"Daniel\"}");
+
+                //PUT for updates
+                 client.Documents.PutAsync("1", "docRevision", "{\"name\":\"Daniel Wertheim\"}");
+
+                //PUT for updates with _rev in JSON
+                 client.Documents.PutAsync("1", "{\"_rev\": \"docRevision\", \"name\":\"Daniel Wertheim\"}");
+
+                //Using entities
+                var me = new Person { Id = "2", Name = "Prathesh" };
+                client.Entities.PutAsync(me);
+
+                //Using anonymous entities
+                client.Entities.PostAsync(new { Name = "George" });
+
+
+                var n = client.Documents.GetAsync("2");
+            }
+        }
+    }
+}
